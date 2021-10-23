1446eeb33af89472116a51b67e228ee3fe5e79aa
viktor44 <viktorbrenek@gmail.com>
2021-10-23 18:06:58 +0200

    Changed the visuals, added items for shroomers and some custom buildables, items etc.

--- JotunnModStub/Spawners.cs
+++ JotunnModStub/Spawners.cs
@@ -1,8 +1,8 @@
-﻿// JotunnModStub
+﻿// ViktorSpawners
 // a Valheim mod skeleton using Jötunn
 // 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
+// File:    ViktorSpawners.cs
+// Project: ViktorSpawners
 
 using BepInEx;
 using Jotunn.Entities;
@@ -12,7 +12,7 @@ using HarmonyLib;
 using Jotunn.Utils;
 
 
-namespace JotunnModStub
+namespace Spawners
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
@@ -43,6 +43,11 @@ namespace JotunnModStub
 
             LoadAssetBundle();
 
+
+            UnloadAssetBundle();
+
+            
+
         }
 
         private void FejdStartup_Awake(On.FejdStartup.orig_Awake orig, FejdStartup self)
@@ -59,13 +64,83 @@ namespace JotunnModStub
          private void LoadAssetBundle()
         {
             // Load asset bundle from embedded resources
+           
             Jotunn.Logger.LogInfo($"Embedded resources: {string.Join(",", typeof(Spawners).Assembly.GetManifestResourceNames())}");
-            _embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("spawnerbossone", typeof(Spawners).Assembly);
+            _embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("shroomerspawner", typeof(Spawners).Assembly);
              
                 // Create and add a custom item
-            var eikthyrSpawner = _embeddedResourceBundle.LoadAsset<GameObject>("eikthyrspawner");
-            PrefabManager.Instance.AddPrefab(new CustomPrefab(eikthyrSpawner, true)); 
+            
+            var shroomerSpawner = _embeddedResourceBundle.LoadAsset<GameObject>("shroomerspawner");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(shroomerSpawner, true)); 
+
+            var Shroomer = _embeddedResourceBundle.LoadAsset<GameObject>("Shroomer");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(Shroomer, true)); 
+
+            var Shroomer_ragdoll = _embeddedResourceBundle.LoadAsset<GameObject>("Shroomer_ragdoll");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(Shroomer_ragdoll, true)); 
+
+            var Shroomie = _embeddedResourceBundle.LoadAsset<GameObject>("Shroomie");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(Shroomie, true)); 
+
+            var RedCrystal = _embeddedResourceBundle.LoadAsset<GameObject>("RedCrystal");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(RedCrystal, true)); 
+
+            var Shroomer_throw_projectile = _embeddedResourceBundle.LoadAsset<GameObject>("Shroomer_throw_projectile");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(Shroomer_throw_projectile, true)); 
+
+            var TrophyShroomer = _embeddedResourceBundle.LoadAsset<GameObject>("TrophyShroomer");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(TrophyShroomer, true)); 
+
+            var ShroomerSpear = _embeddedResourceBundle.LoadAsset<GameObject>("ShroomerSpear");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(ShroomerSpear, true)); 
+
+            var Recipe_ShroomerSpear = _embeddedResourceBundle.LoadAsset<GameObject>("Recipe_ShroomerSpear");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(Recipe_ShroomerSpear, true)); 
+
+            var sfx_shroomer_alerted = _embeddedResourceBundle.LoadAsset<GameObject>("sfx_shroomer_alerted");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(sfx_shroomer_alerted, true)); 
+
+            var sfx_shroomer_idle = _embeddedResourceBundle.LoadAsset<GameObject>("sfx_shroomer_idle");
+            PrefabManager.Instance.AddPrefab(new CustomPrefab(sfx_shroomer_idle, true)); 
+
         }
+
+        ////////////////// Tady přidávám recept do Workbenche
+
+        private static void RecipeShroomerSpear(ItemDrop itemDrop)
+        {
+            // Create and add a recipe for the copied item
+            Recipe recipe = ScriptableObject.CreateInstance<Recipe>();
+            recipe.name = "Recipe_ShroomerSpear";
+            recipe.m_item = itemDrop;
+            recipe.m_craftingStation = PrefabManager.Cache.GetPrefab<CraftingStation>("piece_workbench");
+            recipe.m_resources = new Piece.Requirement[]
+            {
+                    new Piece.Requirement()
+                    {
+                        m_resItem = PrefabManager.Cache.GetPrefab<ItemDrop>("RedCrystal"),
+                        m_amount = 4
+                    },
+                    new Piece.Requirement()
+                    {
+                        m_resItem = PrefabManager.Cache.GetPrefab<ItemDrop>("Wood"),
+                        m_amount = 10
+                    },
+                    new Piece.Requirement()
+                    {
+                        m_resItem = PrefabManager.Cache.GetPrefab<ItemDrop>("LeatherScraps"),
+                        m_amount = 2
+                    }
+            };
+
+            // Since we got the prefabs from the cache, no referencing is needed
+            CustomRecipe CR = new CustomRecipe(recipe, fixReference: false, fixRequirementReferences: false);
+            ItemManager.Instance.AddRecipe(CR);
+        }
+
+        ////////////////// Tady přidávám do Hammeru Spawnery
+        
+        
 
         private void UnloadAssetBundle()
         {
