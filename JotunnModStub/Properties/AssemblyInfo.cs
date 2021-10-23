dc58f659c1e10517a07a954c61818e36358747ac
Nicholas Dunnaway <nick@dunnaway.org>
2021-10-23 13:42:52 -0700

    DR Update

--- JotunnModStub/Properties/AssemblyInfo.cs
+++ JotunnModStub/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-﻿using System.Reflection;
+﻿using Sporelings;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JotunnModStub")]
+[assembly: AssemblyTitle(Shroomer.PluginName)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JotunnModStub")]
+[assembly: AssemblyProduct(Shroomer.PluginName)]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion(Shroomer.PluginVersion)]
+[assembly: AssemblyFileVersion(Shroomer.PluginVersion)]
